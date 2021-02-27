7d2100b006971e2d28b6a07f2864c8c4cecb021e
Dheeraj bisht <48381903+deebisht98@users.noreply.github.com>
2021-02-27 11:59:55 +0530

    Update youtube video downloader.py

--- youtube video downloader.py
+++ youtube video downloader.py
@@ -2,22 +2,29 @@ import tkinter as tk
 import pytube
 from tkinter import messagebox as mb
 from PIL import Image, ImageTk
+from tkinter.messagebox import askyesno
 
 root = tk.Tk()
 root.title('YouTube Downloader')
 canvas = tk.Canvas(root, width = 0, height = 200)
-canvas.grid(column = 5, row = 6)
+canvas.grid(column = 6, row = 6)
 
 logo = Image.open("logo.png")
 logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image = logo)
 logo_label.image = logo
-logo_label.grid(column=3 , row=1)
+logo_label.grid(columnspan=6 , row=1)
 
 inputt = tk.StringVar()
 
 instructions = tk.Label(root, text= "ENTER THE YOUTUBE URL HERE", font = ('Raleway', 15, 'bold'))
 instructions.grid(column = 3, row = 5)
+
+def ask():
+    answer = askyesno(title='confirmation', message='Download Again?')
+    if (answer == False):
+        root.destroy()
+
 
 def song_file():
     link = inputt.get()
@@ -26,6 +33,7 @@ def song_file():
     stream = yt.streams.filter(only_audio=True).first()
     stream.download(r'E:\output\song')
     mb.showinfo("YAY!!!!", "Audio downloaded")
+    ask()
 
 
 def video_file():
@@ -35,6 +43,7 @@ def video_file():
     stream = yt.streams.first()
     stream.download(r'E:\output\video')
     mb.showinfo("YAY!!!!", "Video downloaded")
+    ask()
 
 entry = tk.Entry(root, textvariable=inputt, font=('Raleway', 12, 'normal'), width = 30)
 entry.grid(column=3, row=2)
@@ -47,10 +56,6 @@ video_btn.grid(row=6, column=2)
 song = tk.StringVar()
 audio_btn = tk.Button(root, textvariable=song ,command = lambda:song_file(), font = ('Raleway', 12, 'bold'), bg="#858585", fg = "white" , height = 2, width = 8,relief = "groove")
 song.set("AUDIO")
-audio_btn.grid(row=6, column=3)
-
-exit_button = tk.Button(root, text="EXIT", command=root.destroy, font = ('Raleway', 12, 'bold'), bg="#858585", fg ="white", height = 2, width = 8,relief = "groove")
-exit_button.grid(column = 4, row =6)
-
+audio_btn.grid(row=6, column=4)
 
 root.mainloop()
